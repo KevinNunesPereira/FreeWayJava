59533b04f665c8ee62bbcec2b8ecf1349bed1cdc
KevinNunesPereira <lordzero237@gmail.com>
2023-03-09 12:31:47 -0300

    Arquivos do jogo

---
+++ sketch.js
@@ -0,0 +1,33 @@
+function setup() {
+  createCanvas(500, 400);
+}
+
+function draw() {
+  background(imagemDaEstrada);
+  mostraAtor();
+  mostraCarro();
+  movimentaCarro();
+  movimentaAtor();
+  voltaPosicaoInicialDoCarro();
+  verificaColisao();
+  incluiPontos();
+  marcaPonto();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
